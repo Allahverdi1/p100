9916856fbf0bcf9b32512001ff14cb54c3ff1155
Allahverdi1 <99805939+Allahverdi1@users.noreply.github.com>
2022-04-05 02:22:49 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    internal class Book
+    {
+        public enum GenreType
+        {
+            Genre
+        }
+        public GenreType Genre { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public double Price { get; set; }
+        private static int _no;
+        
+        public int No { get; }
+
+        public Book()
+        {
+            _no++;
+            No = _no;
+        }
+
+
+
+
+    }
+}
---
+++ Library.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    internal class Library
+    {
+        List<Book> books = new List<Book>();
+
+        public List<Book> FilterByPrice(int minPrice, int maxPrice)
+        {
+            return books.FindAll(filter => filter.Price > minPrice && filter.Price < maxPrice);
+        }
+
+        public List<Book> FilterByGenre(string genre)
+        {
+            return books.FindAll(filter => filter.genre == Genre);
+
+        }
+        public Book FindBookByNo(int no)
+        {
+            foreach (var item in books)
+            {
+                if (item.No == no)
+                {
+                    return books.FindAll(find => find.no == books);
+                }
+
+
+
+            }
+            return null;
+
+        }
+    }
+
+      
+}
