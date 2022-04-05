769008fb4e3a5a1d0cff0dce5d6842596d946169
Allahverdi1 <99805939+Allahverdi1@users.noreply.github.com>
2022-04-06 01:58:09 +0300

    Add files via upload

---
+++ Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    internal class Order
+    {
+        public string No { get; set; }
+        public int ProductCount { get; set; }
+        public int TotalPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Shop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp11
+{
+    internal class Shop
+    {
+        List<Order> orders = new List<Order>();
+        public void AddOrder(Order order)
+        {
+            orders.Add(order);
+        }
+        public List<Order> FilterOrderByPrice(int minPrice, int maxPrice)
+        {
+            return orders.FindAll(filter => filter.TotalPrice > minPrice && filter.TotalPrice < maxPrice);
+        }
+        public void RemoveOrderByNo(string no)
+        {
+            if (!string.IsNullOrWhiteSpace(no))
+            {
+                throw new NullReferenceException("Null-dır");
+            }
+            var result = orders.Find(r => r.No == no);
+            if (no == null)
+            {
+                throw new NullReferenceException("Null-dir");
+            }
+            orders.Remove(result);
+        }
+
+
+            
+        
+    }
+}
